52f24ae17b7b0a2a0d8bac59169d5fda9d815e48
Harp <harploidsoft@gmail.com>
2025-09-03 00:21:46 +0100

    perf(db): add explain analyze for booking query

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -11,3 +11,7 @@ CREATE INDEX idx_properties_property_id ON Properties(property_id);
 CREATE INDEX idx_properties_host_id ON Properties(host_id);
 CREATE INDEX idx_properties_location ON Properties(location);
 CREATE INDEX idx_properties_pricepernight ON Properties(price_per_night);
+
+EXPLAIN ANALYZE
+SELECT * FROM Bookings
+WHERE user_id = '660e8400-e29b-41d4-a716-446655440008'
