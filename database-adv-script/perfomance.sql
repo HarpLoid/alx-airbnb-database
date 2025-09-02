5157b18ab6a5d7b70adeef9eb3d63d436049ef42
Harp <harploidsoft@gmail.com>
2025-09-03 00:19:13 +0100

    perf(db): refine performance queries and add explain analyze
    Replaced SELECT * with explicit column selections and added WHERE
    clauses to filter bookings by status and location. Included EXPLAIN
    ANALYZE for the initial query to aid in performance profiling.
    Reduced selected columns in the refactored query for efficiency.

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,30 +1,65 @@
 -- Before Refactoring
-SELECT *
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date,
+    pay.payment_method
 FROM Bookings b
 JOIN Users u ON b.user_id = u.user_id
 JOIN Properties p ON b.property_id = p.property_id
-LEFT JOIN Payments pay ON pay.booking_id = b.booking_id
-ORDER BY b.start_date;
+LEFT JOIN Payments pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+  AND p.location = 'Lagos';
+
+-- EXPLAIN QUERY
+EXPLAIN ANALYZE
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date,
+    pay.payment_method
+FROM Bookings b
+JOIN Users u ON b.user_id = u.user_id
+JOIN Properties p ON b.property_id = p.property_id
+LEFT JOIN Payments pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+  AND p.location = 'Lagos';
 
 -- After Refactoring
-SELECT
-b.booking_id,
-b.start_date,
-b.end_date,
-b.total_price AS booking_total,
-b.status AS booking_status,
-u.user_id,
-u.first_name,
-u.last_name,
-u.email,
-p.property_id,
-p.name AS property_name,
-p.location AS property_location,
-pay.payment_id,
-pay.amount AS payment_amount,
-pay.payment_date
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.first_name,
+    u.last_name,
+    p.name AS property_name,
+    pay.amount
 FROM Bookings b
 JOIN Users u ON b.user_id = u.user_id
 JOIN Properties p ON b.property_id = p.property_id
-LEFT JOIN Payments pay ON pay.booking_id = b.booking_id
-ORDER BY b.start_date;
+LEFT JOIN Payments pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+  AND p.location = 'Lagos';
